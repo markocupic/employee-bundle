8a7021535e35d5aef16724f34d59ed2df7d073c1
Marko Cupic <m.cupic@gmx.ch>
2020-10-14 14:57:50 +0200

    Update default.php

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -14,3 +14,12 @@
 $GLOBALS['TL_LANG']['CTE']['employee_elements'] = 'Mitarbeiter Inhaltselemente';
 $GLOBALS['TL_LANG']['CTE']['employee_list_element'] = ['Mitarbeiter-Liste', 'Fügen Sie dem Layout eine Mitarbeiter-Liste hinzu.'];
 $GLOBALS['TL_LANG']['CTE']['employee_reader_element'] = ['Mitarbeiter-Detailansicht', 'Fügen Sie dem Layout eine Detailansicht des Mitarbeiters hinzu.'];
+
+/**
+ * MSC
+ */
+$GLOBALS['TL_LANG']['MSC']['eb_contact'] = 'Kontakt';
+$GLOBALS['TL_LANG']['MSC']['eb_contactInfo'] = 'Bitte kontaktieren Sie';
+$GLOBALS['TL_LANG']['MSC']['eb_emplyeeInfo'] = 'Infos zur Person';
+$GLOBALS['TL_LANG']['MSC']['eb_officeHours'] = 'Bürozeiten';
+$GLOBALS['TL_LANG']['MSC']['eb_publications'] = 'Publikationen';
